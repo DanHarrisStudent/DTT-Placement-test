f13a0cdff218f1a182c5eca8eaf740683bc1ecf6
Dan-PC\Dan <daniel.harris-4@students.plymouth.ac.uk>
2018-03-07 12:08:01 +0000

    Closed grid create
    Sufficent code created to produce a closed grid

---
+++ PerfectMaze/Assets/Scripts/MazeCell.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeCell : MonoBehaviour {
+
+    //Bool to check if a cell has been visited
+    public bool m_isVisited = false;
+    //reference each game object
+    public GameObject northWall, southWall, eastWall, wastWall, floor; 
+
+}
---
+++ PerfectMaze/Assets/Scripts/MazeGen.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGen : MonoBehaviour
+{
+    //Variables for the Generation script
+    public GameObject Wall;
+    public int m_WallX = 5;
+    public int m_WallY = 5;
+    public float m_MazeSize = 1.0f;//Length of the cube z axis
+
+    private Vector3 m_StartPosition;
+    
+
+    void Start()
+    {
+        GenMaze();
+    }
+    private void GenMaze()
+    {
+        m_StartPosition = new Vector3((-m_WallX / 2) + m_MazeSize / 2, 0.0f, (-m_WallY / 2) + m_MazeSize / 2);
+        Vector3 m_CellPos = m_StartPosition;
+        GameObject tempWall;
+
+        //Walls for X Axis
+        for (int i = 0; i < m_WallY; i++)//For each row (x Axis)
+        {
+            for (int j = 0; j <= m_WallX; j++)//for each column(y Axis)
+            {
+                m_CellPos = new Vector3(m_StartPosition.x + (j * m_MazeSize) - m_MazeSize / 2, 0.0f, m_StartPosition.z + (i * m_MazeSize) - m_MazeSize / 2);
+                Instantiate(Wall,m_CellPos,Quaternion.identity);//Spawn game object
+            }
+        }
+        //Walls for Y Axis
+        for (int i = 0; i <= m_WallY; i++)//For each row (x Axis)
+        {
+            for (int j = 0; j < m_WallX; j++)//for each column(y Axis)
+            {
+                m_CellPos = new Vector3(m_StartPosition.x + (j * m_MazeSize), 0.0f, m_StartPosition.z + (i * m_MazeSize) - m_MazeSize);
+                tempWall = Instantiate(Wall, m_CellPos, Quaternion.Euler(0.0f, 90.0f, 0.0f)) as GameObject;//Spawn wall as a new game object
+            }
+        }
+    }
+
+}
+
+
